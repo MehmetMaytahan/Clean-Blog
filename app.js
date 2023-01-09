2d39082e5b1aa0db5b53aa2230188c080642d627
Mehmet Maytahan <maytahan71@gmail.com>
2023-01-09 11:37:11 +0300

    dinamik site icin temel yapilar olusturuldu.

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require("express");
-const app = express();
 const path = require("path");
-const PORT = 3000;
+const ejs = require("ejs");
 
+const app = express();
+
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+// MIDDLEWARE
 app.use(express.static("public"));
 
+// ROUTES
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("index");
 });
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/home", (req, res) => {
+  res.render("post");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add_post");
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
