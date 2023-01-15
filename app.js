fd87c6e6af68ce9a800dc2ff82b5f8c06decc6de
Mehmet Maytahan <maytahan71@gmail.com>
2023-01-15 19:24:44 +0300

    post detay sayfasi, post guncelleme ve silme islemleri eklendi

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const ejs = require("ejs");
+const methodOverride = require("method-override");
 
 const app = express();
 
 const Post = require("./modals/Post");
+const {
+  getAllPosts,
+  getPost,
+  createPost,
+  updatePost,
+  deletePost
+} = require("./controllers/postController");
+const {
+  aboutPage,
+  homePage,
+  addPage,
+  editPage
+} = require("./controllers/pageController");
 
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
@@ -14,34 +28,18 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
 // ROUTES
-app.get("/", async (req, res) => {
-  const posts = await Post.find({}).sort("-dateCreated");
-  res.render("index", { posts });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-app.get("/home", (req, res) => {
-  res.render("index");
-});
-
-app.get("/add", (req, res) => {
-  res.render("add_post");
-});
-
-app.post("/add", async (req, res) => {
-  await Post.create(req.body);
-  res.redirect("/");
-});
-
-app.get("/post/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render("post-detail", { post });
-});
+app.get("/", getAllPosts); // home page
+app.get("/post/:id", getPost); // post detail page
+app.post("/add", createPost); // add new post
+app.put("/post/:id", updatePost); // update post
+app.delete("/post/:id", deletePost); // delete post
+app.get("/home", homePage); // home page
+app.get("/about", aboutPage); // about page
+app.get("/add", addPage); // add post page
+app.get("/post/edit/:id", editPage); // edit post page
 
 mongoose.set("strictQuery", false);
 mongoose.connect(
